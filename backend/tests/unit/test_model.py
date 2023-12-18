c7e824eee6bef09e9388b31dcebc518f1917482a
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-12-18 15:15:15 +0100

    Furark

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -7,4 +7,15 @@ def test_create_character():
     WHEN a new Character is created
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow (Done)
+    alias = 'hero123'
+    name = 'HeroName'
+    character = Character(alias, name)
+
+    assert character.alias == alias
+    assert character.name == name
+    assert character.level == 1
+    assert character.health == 100.0
+    assert character.strength == 5.0
+    assert character.defense == 5.0
+    assert character.speed == 5.0
