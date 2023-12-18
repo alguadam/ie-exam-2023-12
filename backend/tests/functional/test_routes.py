01c9dcc4dccc0b215ce3f2f4ce4d75251a6f147e
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2023-12-18 14:39:29 +0100

    add functional and unit tests

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,21 +1,24 @@
 from character_api import app
 import pytest
 
-def test_dummy_wrong_path():
+def test_dummy_wrong_path(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/wrong_path' page is requested (GET)
     THEN check the response is valid
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.get('/wrong_path')
+    assert response.status_code == 404
 
 def test_get_characters(testing_client):
     """
-    GIVEN a Flask application
+    GIVEN a Flask application 
     WHEN the '/characters' page is requested (GET)
     THEN check the response is valid
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.get('/characters')
+    assert response.status_code == 200
+    # Add more assertions to validate the response content, if needed
 
 def test_create_character(testing_client):
     """
@@ -24,5 +27,5 @@ def test_create_character(testing_client):
     THEN check the response is valid with status code 200
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
-
-
+    valid_response = {"name": "Test", "alias": "Test", "level": 1, "health": 100, "strength": 10, "defense": 10, "speed": 10}
+    response = testing_client.post('/characters', json=valid_response)
--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -7,4 +7,15 @@ def test_create_character():
     WHEN a new Character is created
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    # Create a new Character
+    character = Character(alias="Test", name="Test", level=10, health=100, strength=50, defense=30, speed=80)
+
+    # Check if the attributes are defined correctly
+    assert character.alias == "Test"
+    assert character.name == "Test"
+    assert character.level == 10
+    assert character.health == 100
+    assert character.strength == 50
+    assert character.defense == 30
+    assert character.speed == 80
+    
