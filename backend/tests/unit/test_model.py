69a40ee1c3c1c88b731900b60f773e4a5c201812
Hussein Soliman <127189589+HusseinnSolimann@users.noreply.github.com>
2023-12-18 15:47:52 +0100

    First deployment

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -7,4 +7,15 @@ def test_create_character():
     WHEN a new Character is created
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
+    
+    character = Character('hmahmoud', 'hussein')
+    assert character.alias == 'hmahmoud'
+    assert character.name == 'hussein'
+    assert character.level == 1
+    assert character.health == 100.0
+    assert character.strength == 5.0
+    assert character.defense == 5.0
+    assert character.speed == 5.0
+
+    
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
