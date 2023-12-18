73bc6529bafce23a5164c8c50870b8f05575826e
cristina requena <137072659+cristinareq@users.noreply.github.com>
2023-12-18 16:24:07 +0100

    Update test_routes.py

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,6 +1,12 @@
 from character_api import app
 import pytest
 
+# ok now we just created functional tests and we have three of them
+# the first one is just sending it purposefully to the wrong path and checking if we don't get a valid path, give 404 so that's that
+# then for get characters we simple send a get request at /characters which gets all the characters, and if works 200 status code so pass.
+# then for create character we have a post request where again we only put the values that we need to, and then if it works 200 so pass.
+
+
 def test_dummy_wrong_path():
     """
     GIVEN a Flask application
@@ -8,9 +14,9 @@ def test_dummy_wrong_path():
     THEN check the response is valid
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
-
-    response = testing_client.get('/wrong_path')
-    assert response.status_code == 404
+    with app.test_client() as client:
+        response = client.get('/wrong_path')
+        assert response.status_code == 404
 
 def test_get_characters(testing_client):
     """
@@ -19,11 +25,10 @@ def test_get_characters(testing_client):
     THEN check the response is valid
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
-
     response = testing_client.get('/characters')
     assert response.status_code == 200
-    
-    
+
+
 def test_create_character(testing_client):
     """
     GIVEN a Flask application
@@ -31,7 +36,5 @@ def test_create_character(testing_client):
     THEN check the response is valid with status code 200
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
-
-    response = testing_client.post(
-        '/characters', json={'alias': 'johndoe_alias', 'name': 'John Doe'})
+    response = testing_client.post('/characters', json={'alias': 'johndoe_alias', 'name': 'John Doe'})
     assert response.status_code == 200
