a8d9541a1c91f667474f85e39f3cade88cd2fb68
papilio a. <126614634+papilioaureus@users.noreply.github.com>
2023-12-18 15:06:43 +0000

    will i have to bathe in the blood of a virgin to get this deployment to work???

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,13 +1,20 @@
 from character_api import app
 import pytest
 
-def test_dummy_wrong_path():
+def test_dummy_wrong_path(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/wrong_path' page is requested (GET)
     THEN check the response is valid
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    #Define the endpoint
+    endpoint = '/wrong_path'
+
+    #Send a GET request to the endpoint
+    response = testing_client.get(endpoint)
+
+    #Assert that the response status code is 404 (Not Found)
+    assert response.status_code == 404
 
 def test_get_characters(testing_client):
     """
@@ -15,7 +22,17 @@ def test_get_characters(testing_client):
     WHEN the '/characters' page is requested (GET)
     THEN check the response is valid
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    #Define the endpoint
+    endpoint = '/characters'
+
+    #Send a GET request to the endpoint
+    response = testing_client.get(endpoint)
+
+    #Assert that the response status code is 200 (OK)
+    assert response.status_code == 200
+
+    #Assert that the response data is a list (this assumes that the endpoint returns a list of characters)
+    assert isinstance(response.get_json(), list)
 
 def test_create_character(testing_client):
     """
@@ -23,6 +40,32 @@ def test_create_character(testing_client):
     WHEN the '/characters' page is posted to (POST) with a valid body
     THEN check the response is valid with status code 200
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    #Define the endpoint the functional test will access
+    endpoint = '/characters'
 
+    #Define the character's body
+    body = {
+        'alias': 'Test Alias',
+        'name': 'Test Name',
+        'level': 1,
+        'health': 100,
+        'strength': 10,
+        'defense': 10,
+        'speed': 10
+    }
 
+    #Send a POST request to the endpoint
+    response = testing_client.post(endpoint, json=body)
+
+    #Assert that the response status code = 200
+    assert response.status_code == 200
+
+    #Assert that the response data is as expected
+    response_data = response.get_json()
+    assert response_data['alias'] == 'Test Alias'
+    assert response_data['name'] == 'Test Name'
+    assert response_data['level'] == 1
+    assert response_data['health'] == 100
+    assert response_data['strength'] == 10
+    assert response_data['defense'] == 10
+    assert response_data['speed'] == 10
--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -7,4 +7,12 @@ def test_create_character():
     WHEN a new Character is created
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    character = Character(alias='Test Alias', name='Test Name', level=1, health=100, strength=10, defense=10, speed=10)
+    # Assert that the attributes are as expected
+    assert character.alias == 'Test Alias'
+    assert character.name == 'Test Name'
+    assert character.level == 1
+    assert character.health == 100
+    assert character.strength == 10
+    assert character.defense == 10
+    assert character.speed == 10
