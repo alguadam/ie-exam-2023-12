bf31a69c39b5f7540220ba47c4df8eaecec4d2d1
Joseph-233 <116306347+Joseph-233@users.noreply.github.com>
2023-12-18 15:32:51 +0100

    Initial commit to see if workflows run

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -8,3 +8,11 @@ def test_create_character():
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    character = Character(alias='Juni', name='Joseph', level=1, health=1, strength=1, defense=1, speed=1)
+    assert character.alias == 'Juni'
+    assert character.name == 'Joseph'
+    assert character.level == 1
+    assert character.health == 1
+    assert character.strength == 1
+    assert character.defense == 1
+    assert character.speed == 1
