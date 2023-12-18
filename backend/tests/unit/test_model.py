05438b036249fefc21384e538d0a38f721b74b9d
Khaled Akel <khaled-akel@outlook.com>
2023-12-18 15:38:11 +0100

    khaled

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -8,3 +8,12 @@ def test_create_character():
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    char = Character('kakel', 'khaled')
+    assert char.alias == 'kakel'
+    assert char.name == 'khaled'
+    assert char.level == 1
+    assert char.health == 100.0
+    assert char.strength == 5.0
+    assert char.defense == 5.0
+    assert char.speed == 5.0
+    
