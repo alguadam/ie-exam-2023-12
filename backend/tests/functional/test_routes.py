c4e22b5d6948ca50cbc26dfd98c59a7ee65c162e
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2023-12-18 16:16:05 +0100

    xxx

---
+++ backend/tests/functional/test_routes.py
@@ -0,0 +1,35 @@
+from character_api import app
+import pytest
+
+def test_dummy_wrong_path():
+    """
+    GIVEN a Flask application
+    WHEN the '/wrong_path' page is requested (GET)
+    THEN check the response is valid
+    """
+    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    with app.test_client() as client:
+        response = client.get('/wrong_path')
+        assert response.status_code == 404
+
+def test_get_characters(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/characters' page is requested (GET)
+    THEN check the response is valid
+    """
+    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.get('/characters')
+    assert response.status_code == 200
+
+def test_create_character(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/characters' page is posted to (POST) with a valid body
+    THEN check the response is valid with status code 200
+    """
+    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.post('/characters', json={'alias': 'xx', 'name': 'name', 'level': '100', 'health': '10.0', 'strength': '10.0', 'defense': '10.0', 'speed': '10.0'})
+    assert response.status_code == 200
+
+
