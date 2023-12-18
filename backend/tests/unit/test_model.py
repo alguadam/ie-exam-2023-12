7b1902cd442e72aa8e19eff942695e7e79675f9e
Zaid Dakhqan <zdakhqan.ieu2021@student.ie.edu>
2023-12-18 15:48:03 +0100

    testing py - zaid

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -8,11 +8,11 @@ def test_create_character():
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
-    new_character = Character(alias='Do7', name='Zaid', level=0, health=100, strength=10, defense=10, speed=10)
+    new_character = Character(alias='Do7', name='Zaid', level=1, health=100, strength=10, defense=10, speed=10)
     assert new_character.alias == 'Do7'
     assert new_character.name == 'Zaid'
-    assert new_character.level == 0
-    assert new_character.health == 100
-    assert new_character.strength == 10
-    assert new_character.defense == 10
-    assert new_character.speed == 10
+    assert new_character.level == 1.0
+    assert new_character.health == 100.0
+    assert new_character.strength == 10.0
+    assert new_character.defense == 10.0
+    assert new_character.speed == 10.0
