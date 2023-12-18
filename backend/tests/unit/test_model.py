cc357e99d7c00022f455f08e8af6f2edf34fcf09
Adnan Bhanji <81592934+adnanbhanji@users.noreply.github.com>
2023-12-18 14:37:05 +0100

    Update test_model.py

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -8,3 +8,12 @@ def test_create_character():
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    
+    character = Character('johnuser', 'John Doe', 1,  'Spain', '€')
+    assert character.name == 'John Doe'
+    assert character.country == 'Spain'
+    assert character.currency == '€'
+    assert character.account_number != None
+    assert character.balance == 0.0
+    assert character.status == 'Active'
+
