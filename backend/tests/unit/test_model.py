cf451158dfc5064e9c61ec5da95ba9a687efdf31
beaamoo <beatrice.mossberg@gmail.com>
2023-12-18 16:21:16 +0100

    test fix

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -7,11 +7,11 @@ def test_create_character():
     WHEN a new Character is created
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
-    character = Character('bea', 'Beatriz')
+    character = Character(alias="bea", name="Beatriz")
     assert character.alias == 'bea'
     assert character.name == 'Beatriz'
     assert character.level == 1
     assert character.health == 100
-    assert character.strength == 10
-    assert character.defense == 10
-    assert character.speed == 10
+    assert character.strength == 5
+    assert character.defense == 5
+    assert character.speed == 5
