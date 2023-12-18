277946934aaf75ae48ba9b8d3a1313ea84959a77
Liubey135 <104011587+Liubey135@users.noreply.github.com>
2023-12-18 14:39:53 +0100

    Update test_model.py

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -2,9 +2,14 @@ from character_api.models import Character
 import pytest
 
 def test_create_character():
-    """
-    GIVEN a Character model
-    WHEN a new Character is created
-    THEN check the alias, name, level, health, strength, defense and speed are defined correctly
-    """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+
+    character = Character(alias="Hero", name="John Doe", level=1, health=100, strength=10, defense=5, speed=3)
+
+    # Assertions to check if each attribute is set correctly
+    assert character.alias == "Hero"
+    assert character.name == "John Doe"
+    assert character.level == 1
+    assert character.health == 100
+    assert character.strength == 10
+    assert character.defense == 5
+    assert character.speed == 3
