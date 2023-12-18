649208d72bc687e3a2487d2daf75325df35b9f84
Liubey135 <104011587+Liubey135@users.noreply.github.com>
2023-12-18 14:49:13 +0100

    Update test_routes.py

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,28 +1,28 @@
 from character_api import app
 import pytest
 
-def test_dummy_wrong_path():
-    """
-    GIVEN a Flask application
-    WHEN the '/wrong_path' page is requested (GET)
-    THEN check the response is valid
-    """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+def testing_client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_dummy_wrong_path(testing_client):
+    response = testing_client.get('/wrong_path')
+    assert response.status_code == 404  
+
 
 def test_get_characters(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/characters' page is requested (GET)
-    THEN check the response is valid
-    """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    response = testing_client.get('/characters')
+    assert response.status_code == 200
+
 
 def test_create_character(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/characters' page is posted to (POST) with a valid body
-    THEN check the response is valid with status code 200
-    """
-    # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    valid_payload = {
+        "alias": "Hero",
+        "name": "John Doe",
+    }
+    response = testing_client.post('/characters', json=valid_payload)
+    assert response.status_code == 200
+
 
 
