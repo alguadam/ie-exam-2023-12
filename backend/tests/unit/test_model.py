200c8c89ab25d280717e96801b533621fb49b142
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-12-18 14:43:01 +0100

    functional and unit tests done

--- backend/tests/unit/test_model.py
+++ backend/tests/unit/test_model.py
@@ -8,3 +8,12 @@ def test_create_character():
     THEN check the alias, name, level, health, strength, defense and speed are defined correctly
     """
     # Exercise 1: Develop this test according to the definition and make it pass in the GitHub workflow
+    new_character = Character(alias='hero123', name='Hero')
+
+    assert new_character.alias == 'hero123'
+    assert new_character.name == 'Hero'
+    assert new_character.level == 1
+    assert new_character.health == 100.0
+    assert new_character.strength == 5.0
+    assert new_character.defense == 5.0
+    assert new_character.speed == 5.0
